c69d12ba1f092c5b9f8d82296f603ace081aee87
leo900807 <leo900807@yahoo.com.tw>
2024-01-06 15:15:47 +0800

    Finish main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]){
         OUTPUT_FILENAME = argv[2];
     }
 
-    AudioFile<double> audio_file;
+    AudioFile<AudioType> audio_file;
     bool load_success = audio_file.load(argv[1]);
     if(!load_success){
         cerr << "Audiofile load error\n";
@@ -28,21 +28,9 @@ int main(int argc, char *argv[]){
 
     vector<AudioType> samples = sample_conversion(audio_file.samples, num_channels, num_samples, length_in_seconds);
 
-    vector<cv::Mat> images = image_generation(samples);  // TODO
+    cv::Mat images = image_generation(samplesi, 0, samples.size());
 
-    vector<cv::Mat> video = image_concat(images);  // TODO
-
-    cv::VideoWriter output;
-    output.open(OUTPUT_FILENAME, EX, FPS, FRAME_SIZE, false);
-
-    if(!output.isOpened()){
-        cerr << "Could not open the output video for write\n";
-        return 1;
-    }
-
-    for(cv::Mat frame : video){
-        output << frame;
-    }
+    concat_images(images);  // Output included
 
     cout << "Done\n";
 
