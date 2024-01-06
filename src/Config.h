98b79393a8209ccf317a31504efe02533e23192f
leo900807 <leo900807@yahoo.com.tw>
2024-01-06 14:06:44 +0800

    Add Config.h

---
+++ src/Config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <opencv2/videoio.hpp>
+#include <string>
+
+using AudioType = double;
+
+std::string OUTPUT_FILENAME = "output.mp4";
+
+int FPS = 30;
+
+int EX = cv::VideoWriter::fourcc('M', 'P', '4', 'V');
+
+int WIDTH = 1920;
+
+int HEIGHT = 1080;
+
+cv::Size FRAME_SIZE = Size(WIDTH, HEIGHT);
