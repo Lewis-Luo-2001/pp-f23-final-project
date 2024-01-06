6d2aae49a801f9877c3949a71b5fc82f8fdc8bd2
auyu0408 <datealive102164@gmail.com>
2024-01-06 17:07:53 +0800

    fix: fix rows and cols

--- src/ImageGeneration.cpp
+++ src/ImageGeneration.cpp
@@ -2,13 +2,14 @@
 
 cv::Mat image_generation(std::vector<AudioType> samples, int begin, int end){
     int bar_width = (WIDTH) / BAR_IN_A_FRAME;
-    cv::Mat res = cv::Mat((bar_width) * (end-begin), HEIGHT, CV_8UC1, cv::Scalar(255));
+    cv::Mat res = cv::Mat(HEIGHT, (bar_width) * (end-begin), CV_8UC1, cv::Scalar(255));
 
     for(int i = begin; i < end; i++) {
         int high = round(samples[i]);
         for(int h = 0; h < high / 2; h++) {
             for(int w = i * bar_width; w < (i + 1) * bar_width; w++) {
                 res.at<uchar>(HEIGHT / 2 + h, w) = 0;
+                res.at<uchar>(HEIGHT / 2 - h, w) = 0;
             }
         }
     }
